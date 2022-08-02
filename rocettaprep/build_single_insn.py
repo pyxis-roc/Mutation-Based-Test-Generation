00e896e7d586461a601f89f31f16b9f3cee38e06
Sreepathi Pai <sree@cs.rochester.edu>
2022-08-02 15:28:35 -0400

    Generate a partial oracle single instruction file

--- rocettaprep/build_single_insn.py
+++ rocettaprep/build_single_insn.py
@@ -8,11 +8,19 @@
 
 
 import argparse
-from pycparser import parse_file
+from pycparser import parse_file, c_ast, c_generator
 import subprocess
 import tempfile
 from pathlib import Path
 import os
+from collections import OrderedDict
+
+class FuncDefVisitor(c_ast.NodeVisitor):
+    def __init__(self):
+        self.fd_nodes = OrderedDict()
+
+    def visit_FuncDef(self, node):
+        self.fd_nodes[node.decl.name] = node
 
 class PTXSemantics:
     EXCLUDE_INDIRECT = {'ptxc_utils_template.h', '128types.h'}
@@ -29,8 +37,14 @@ class PTXSemantics:
         return cpp_args
 
     def parse(self):
-        parse_file(self.csemantics, use_cpp=True, cpp_path=self.cpp,
-                   cpp_args = self._get_cpp_args())
+        self.ast = parse_file(self.csemantics, use_cpp=True, cpp_path=self.cpp,
+                              cpp_args = self._get_cpp_args())
+
+    def get_functions(self):
+        v = FuncDefVisitor()
+        v.visit(self.ast)
+        self.funcdef_nodes = v.fd_nodes
+        return self.funcdef_nodes
 
     def get_headers(self):
         # use the C preprocessor to extract include files
@@ -42,7 +56,7 @@ class PTXSemantics:
         headers = ''.join(open(t, "r").readlines())
         headers = headers.replace('\\\n', '\n')
 
-        # parse the output
+        # parse the output which is formatted as a Make rule
         target = self.csemantics.stem + '.o:'
         assert headers.startswith(target), headers
         headers = headers[len(target):].strip().split() # will have issues with spaces in paths
@@ -51,16 +65,54 @@ class PTXSemantics:
         # keep only those in the same directory
         prefix = self.csemantics.parent
         headers = [x.name for x in headers if x != self.csemantics and x.parent == prefix and x.name not in self.EXCLUDE_INDIRECT]
-        return headers
+
+        self.headers = headers
+        return self.headers
+
+    def create_single_insn_program(self, insn_fn, sys_includes = [], user_includes = []):
+        assert insn_fn in self.funcdef_nodes, f"{insn_fn} not found in {self.csemantics}"
+
+        generator = c_generator.CGenerator()
+        func_code = generator.visit(self.funcdef_nodes[insn_fn])
+
+        out = []
+        out.extend([f'#include <{x}>' for x in sys_includes])
+        out.extend([f'#include "{x}"' for x in user_includes])
+
+        out.append(f'// START: {insn_fn}')
+        out.append(func_code)
+        out.append(f'// END: {insn_fn}')
+
+        return '\n'.join(out)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="Generate single instruction tests from the C semantics")
     p.add_argument("csemantics", help="C semantics file, usually ptxc.c")
+    p.add_argument("outputdir", help="Output directory, will be created if it does not exist")
+
     p.add_argument("--fake-includes", help="Path to pycparser stub includes", default="/usr/share/python3-pycparser/fake_libc_include/") # this default is good for most Debian-based machines
     p.add_argument("-I", dest="include_dirs", help="Include directory for preprocessor", action="append", default=[])
 
     args = p.parse_args()
 
+    if not os.path.exists(args.outputdir):
+        os.mkdir(args.outputdir)
+
     p = PTXSemantics(args.csemantics, [args.fake_includes] + args.include_dirs)
-    #p.parse()
-    print(p.get_headers())
+    p.parse()
+    p.get_functions()
+    hdrs = p.get_headers()
+
+    oroot = Path(args.outputdir)
+
+    for insn in ['add_rm_ftz_f32']:
+        odir = oroot / f'working-directory-{insn}'
+        if not odir.exists():
+            odir.mkdir()
+
+        code = p.create_single_insn_program(f'execute_{insn}',
+                                            ['stdlib.h', 'stdint.h', 'math.h'],
+                                            hdrs)
+
+        with open(odir / f'{insn}.c', "w") as f:
+            f.write(code)
