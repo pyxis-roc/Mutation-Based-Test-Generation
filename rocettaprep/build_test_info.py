242acc0bb83245c8cc87ee8662f9b1f8b4ee766b
Sreepathi Pai <sree@cs.rochester.edu>
2022-08-18 14:25:54 -0400

    Add Makefile.outputs to generate oracle outputs

--- rocettaprep/build_test_info.py
+++ rocettaprep/build_test_info.py
@@ -10,6 +10,7 @@ import argparse
 import yaml
 import json
 
+from roctest import InsnTest
 from rocprepcommon import *
 from build_single_insn import Insn
 
@@ -19,6 +20,23 @@ def extract_insn_test_info(wp, insn_info, insn):
     # json is faster
     with open(od / "testcases.json", "w") as f:
         json.dump(insn_info, fp=f, indent='  ')
+
+def gen_oracle_makefile(wp, insn_info, insn):
+    def output(index, testcase, insn):
+        return testcase['output']
+
+    ti = InsnTest(wp, insn)
+    ti.set_insn_info(insn_info)
+
+    with open(wp.workdir / insn.working_dir / "Makefile.outputs", "w") as f:
+        odirname = "outputs"
+
+        f.write(f"all: {odirname} " + " ".join([testcase['output'] for testcase in insn_info['tests']]) + "\n\n")
+        f.write(f"{odirname}:\n\tmkdir -p $@\n\n")
+
+        for t in ti.gen_tests(output_fn = output):
+            cmd = " ".join([str(c) for c in t.cmdline])
+            f.write(f"{t.tmp_output}: {odirname}\n\t{cmd}\n\n")
 
 if __name__ == "__main__":
     from setup_workdir import WorkParams
@@ -44,3 +62,4 @@ if __name__ == "__main__":
         for i in insns:
             insn = Insn(i)
             extract_insn_test_info(wp, iid[i], insn)
+            gen_oracle_makefile(wp, iid[i], insn)
