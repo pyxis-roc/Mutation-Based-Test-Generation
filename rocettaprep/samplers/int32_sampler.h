dae3130030fbd3b3181cb76e08cff5e99efd000e
Sreepathi Pai <sree@cs.rochester.edu>
2022-08-25 13:12:53 -0400

    Add header files for stratified samplers in the fuzzer

---
+++ rocettaprep/samplers/int32_sampler.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+#include "unisampler.h"
+static uint32_t int32_min_range = 1;
+static uint32_t int32_min(uint32_t index) {
+    index = index % 1;
+    if (index < 1) return index + 4294967295u;
+}
+static uint32_t int32_zero_range = 1;
+static uint32_t int32_zero(uint32_t index) {
+    index = index % 1;
+    if (index < 1) return index + 0u;
+}
+static uint32_t int32_one_range = 1;
+static uint32_t int32_one(uint32_t index) {
+    index = index % 1;
+    if (index < 1) return index + 1u;
+}
+static uint32_t int32_neg_range = 2147483647;
+static uint32_t int32_neg(uint32_t index) {
+    index = index % 2147483647;
+    if (index < 2147483647) return index + 2147483648u;
+}
+static uint32_t int32_pos_range = 2147483645;
+static uint32_t int32_pos(uint32_t index) {
+    index = index % 2147483645;
+    if (index < 2147483645) return index + 2u;
+}
+static uint32_t int32_max_range = 1;
+static uint32_t int32_max(uint32_t index) {
+    index = index % 1;
+    if (index < 1) return index + 2147483647u;
+}
+static int32_t sample_int32() {
+  union bit2value {
+    uint32_t b;
+    int32_t v;
+   } v;
+  uint32_t br;
+  br = uniform_sample(6);
+  switch(br) {
+  case 0:
+      v.b = int32_min(uniform_sample(int32_min_range));
+      break;
+  case 1:
+      v.b = int32_zero(uniform_sample(int32_zero_range));
+      break;
+  case 2:
+      v.b = int32_one(uniform_sample(int32_one_range));
+      break;
+  case 3:
+      v.b = int32_neg(uniform_sample(int32_neg_range));
+      break;
+  case 4:
+      v.b = int32_pos(uniform_sample(int32_pos_range));
+      break;
+  case 5:
+      v.b = int32_max(uniform_sample(int32_max_range));
+      break;
+  }
+   return v.v;
+}
---
+++ rocettaprep/samplers/strata_sample.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/* Inline all the things */
+
+#include "float_sampler.c"
+#include "int32_sampler.h"
