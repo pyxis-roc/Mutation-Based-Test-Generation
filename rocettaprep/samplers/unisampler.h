b1974edfe4919c8ebca7170961deb41248dd2787
Sreepathi Pai <sree@cs.rochester.edu>
2022-08-26 13:07:13 -0400

    Modify uniform_sampler to handle ranges > RAND_MAX (but still less than UINT32_MAX)

--- rocettaprep/samplers/unisampler.h
+++ rocettaprep/samplers/unisampler.h
@@ -1,6 +1,8 @@
 #pragma once
 
-static uint32_t uniform_sample(const uint32_t range) {
+// returns a number [0, range)
+static uint32_t uniform_sample_1(const uint32_t range) {
+  // this samples a range that fits in RAND_MAX
   // http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Randomization.html
   // essentially rejection sampling
   uint32_t n;
@@ -16,4 +18,34 @@ static uint32_t uniform_sample(const uint32_t range) {
   while(n >= limit) n = random();
 
   return n % range;
+}
+
+static uint32_t uniform_sample(const uint32_t range) {
+  uint32_t n;
+
+  if(range == 1) return 0;
+
+  if(range <= RAND_MAX)
+    return uniform_sample_1(range);
+
+  assert(RAND_MAX <= UINT32_MAX);
+
+  uint32_t range_d = range / RAND_MAX;
+  uint32_t range_r = range % RAND_MAX;
+
+  if(range_r > 0) range_d += 1;
+
+  // first pick a range uniformly
+  uint32_t range_id = uniform_sample(range_d);
+
+  // then pick a limit
+  uint32_t limit;
+  if(range_id == (range_d - 1))
+    limit = range_r;
+  else
+    limit = RAND_MAX;
+
+  uint32_t v = uniform_sample(limit);
+
+  return range_id * RAND_MAX + v;
 }
