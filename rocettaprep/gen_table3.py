84a7367a20eeadc31c8868975960b3282765366f
Sreepathi Pai <sree@cs.rochester.edu>
2022-09-20 16:21:23 -0400

    Initial table 3 generation

---
+++ rocettaprep/gen_table3.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import argparse
+import polars as pl
+import ci
+
+if __name__ == "__main__":
+    from setup_workdir import WorkParams
+
+    p = argparse.ArgumentParser(description="Generate Table 3, equivalence checker stats")
+
+    p.add_argument("workdir", help="Work directory")
+    p.add_argument("experiment", help="Experiment name, must be suitable for embedding in filenames")
+    p.add_argument("-o", "--output", help="Output file")
+
+    args = p.parse_args()
+
+    wp = WorkParams.load_from(args.workdir)
+
+    expt_dir = wp.workdir / f'expt.{args.experiment}'
+
+    if not expt_dir.exists():
+        print(f"ERROR: {expt_dir} does not exist", file=sys.stderr)
+        sys.exit(1)
+
+    stats_file = expt_dir / f'stats_inputs.{args.experiment}.csv'
+    if not stats_file.exists():
+        print(f"ERROR: {stats_file} does not exist, make sure you've run stats_inputs.py",
+              file=sys.stderr)
+
+        sys.exit(1)
+
+    timing_file = expt_dir / f'stats_timing_summary.{args.experiment}.csv'
+    if not timing_file.exists():
+        print(f"ERROR: {timing_file} does not exist, make sure you've run stats_timing.py",
+              file=sys.stderr)
+
+        sys.exit(1)
+
+    stats = pl.read_csv(stats_file).filter(pl.col("source") == "eqvcheck")
+    timing = pl.read_csv(timing_file)
+
+    eqvcheck_timing = timing.filter(pl.col("source") == "eqvcheck")
+    stats = stats.join(eqvcheck_timing, on=["experiment", "instruction"])
+
+    stats = stats.with_columns([
+        pl.col('time_ns_count').apply(lambda c: ci.critlevel(c, 95)).alias("_time_ns_critlevel"),
+    ])
+
+    stats = stats.with_columns(
+        [
+            (pl.col('time_ns_avg') / 1E6).alias("time_ms_avg"),
+            pl.struct(["time_ns_count", "time_ns_stdev", "_time_ns_critlevel"]).apply(lambda x: ci.calc_ci_2(x['_time_ns_critlevel'], x['time_ns_stdev'] / 1E6, x['time_ns_count'])).alias("time_ms_ci95")
+        ])
+
+    tbl = stats[["instruction", 'total', 'unique', 'time_ms_avg', 'time_ms_ci95']]
+
+    if args.output:
+        tbl.write_csv(args.output)
+    else:
+        print(tbl)
