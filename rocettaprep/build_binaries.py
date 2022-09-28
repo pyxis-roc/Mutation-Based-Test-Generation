b6d52db03d122a5a0615ef2d8362f9f2c1c31aaa
Sreepathi Pai <sree@cs.rochester.edu>
2022-09-28 10:40:25 -0400

    Get scalability, and restrict make -j to avoid extremely high loads

--- rocettaprep/build_binaries.py
+++ rocettaprep/build_binaries.py
@@ -10,6 +10,7 @@ from rocprepcommon import *
 from parsl.app.errors import BashExitFailure
 from parsl.dataflow.error import DependencyError
 import sys
+import os
 
 @bash_app
 def make_oracle(wp, insn, stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME, label = None):
@@ -17,22 +18,25 @@ def make_oracle(wp, insn, stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME, 
 
 @bash_app
 def make_outputs(oracle, wp, insn, stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME, label = None):
-    return f'make -C {wp.workdir / insn.working_dir} -f Makefile.outputs -j'
+    x = os.cpu_count()
+    return f'make -C {wp.workdir / insn.working_dir} -f Makefile.outputs -j {x}'
 
 @bash_app
 def make_mutants(wp, insn, mutant="music", stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME, label = None):
-    return f'make -C {wp.workdir / insn.working_dir / mutant} -j -k'
+    x = os.cpu_count()
+    return f'make -C {wp.workdir / insn.working_dir / mutant} -j {x} -k'
 
 def get_ba_result(header, ba):
     try:
         # for some reason, this does not catch parsl.app.error.BashAppNoReturn,
         # but it is logged to parsl.log.
 
-        res = x.result()
+        res = ba.result()
         if res == 0:
             print(f"{header}: Success.")
         return True
     except BashExitFailure as e:
+        # exits with code 2 indicate compiler failures. With mutants, this is normal.
         print(f"{header}: Failed with code {e.exitcode}")
     except DependencyError as e:
         print(f"{header}: Workflow failed. Examine logs in runinfo (usually).")
@@ -63,12 +67,17 @@ if __name__ == "__main__":
     for i in insns:
         insn = Insn(i)
         r = make_oracle(wp, insn, label=i)
-        r = make_outputs(r, wp, insn, label=i)
         out.append((i, r))
 
-    for i, x  in out:
-        if get_ba_result(i, x):
-            # run mutants one at a time, since make -j
-            insn = Insn(i)
-            m = make_mutants(wp, insn, label=i)
-            get_ba_result(i + " mutants", m)
+    out2 = []
+    for i, r in out:
+        # also -j
+        r2 = make_outputs(r, wp, insn, label=i)
+        if get_ba_result(i, r2):
+            out2.append(i)
+
+    for i in out2:
+        # run mutants one at a time, since make -j
+        insn = Insn(i)
+        m = make_mutants(wp, insn, label=i)
+        get_ba_result(i + " mutants", m)
