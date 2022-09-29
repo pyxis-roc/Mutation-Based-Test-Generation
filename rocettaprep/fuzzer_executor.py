83e9fe07a4221db2c2493722653712e5ab341b01
Sreepathi Pai <sree@cs.rochester.edu>
2022-09-29 15:10:24 -0400

    Add run_fuzzer_2.py that improves scalability, move fuzzer executor out to aid in unpickling.

---
+++ rocettaprep/fuzzer_executor.py
@@ -0,0 +1,50 @@
+from runcommon import run_and_time
+import sys
+
+class FuzzerExecutor:
+    def __init__(self, wp, experiment, subset = ''):
+        self.wp = wp
+        self.experiment = experiment
+        self.subset = subset
+
+    def make(self, mutant_exec):
+        p = mutant_exec.parent
+        t = mutant_exec.name
+
+        print(f"{mutant_exec}: Compiling executable", file=sys.stderr)
+        r, tm = run_and_time(["make", "-C", str(p), t])
+        print(f"{mutant_exec}: Compilation took {tm/1E6} ms", file=sys.stderr)
+        return r.returncode
+
+    def run(self, insn, mutant):
+        if self.subset:
+            subset = 'all.'
+        else:
+            subset = ''
+
+        # this can make running repeats painful.
+        odir = mutant.parent / f"fuzzer_output.{mutant.name}.{subset}{self.experiment}"
+
+        cmd = [str(mutant), f"-exact_artifact_path={odir}"]
+
+        # run make always to ensure correct binaries
+        r = self.make(mutant)
+        if not (r == 0):
+            print(f"{mutant}:ERROR: Compilation appears to have failed. Continuing anyway.",
+                  file=sys.stderr)
+
+        print(f"{mutant}: {' '.join(cmd)}", file=sys.stderr)
+
+        try:
+            r, t = run_and_time(cmd, timeout_s = 15)
+            if t is not None:
+                print(f"{mutant}:{subset}{self.experiment}: Total fuzzing time {t/1E6} ms, retcode = {r.returncode}", file=sys.stderr)
+            else:
+                print(f"{mutant}:{subset}{self.experiment}: Fuzzing timed out", file=sys.stderr)
+
+            return {'time_ns': t, 'retcode': r.returncode}
+        except FileNotFoundError:
+            print(f"ERROR: {mutant} does not exist.", file=sys.stderr)
+            return None
+
+        assert False
---
+++ rocettaprep/run_fuzzer_2.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+#
+# run_fuzzer_2.py
+#
+# Run fuzzers.
+
+import subprocess
+import argparse
+from roctest import *
+from rocprepcommon import *
+from build_single_insn import Insn
+import json
+import subprocess
+from mutate import MUSICHelper, get_mutation_helper, get_mutators
+import itertools
+import os
+import json
+import sys
+
+from parsl.app.app import python_app, join_app
+import parsl
+
+@python_app
+def run_fuzzer_on_mutant(executor, insn, mutsrc):
+    return executor.run(insn, mutsrc)
+
+@python_app
+def finish_fuzzer(workdir, fuzzer, all_suffix, experiment, programs=[], inputs=[]):
+    import json
+
+    out = list(zip(programs, inputs))
+
+    results = []
+    for p, r in out:
+        if not (r is None):
+            results.append((p, r))
+
+    # fuzzer doesn't have 'results' like eqvcheck?
+    with open(workdir / f"libfuzzer_{fuzzer}_results{all_suffix}.{experiment}.json", "w") as f:
+        json.dump(dict(results), fp=f)
+
+@join_app
+def run_fuzzer(wp, insn, experiment, muthelper, all_mutants = False, fuzzer = 'simple', parallel = True):
+    import json
+    from roctest import InsnTest
+    from fuzzer_executor import FuzzerExecutor
+
+    tt = InsnTest(wp, insn)
+
+    workdir = wp.workdir / insn.working_dir
+
+    mutants = muthelper.get_mutants(insn)
+    survivors = muthelper.get_survivors(insn, experiment)
+
+    executor = FuzzerExecutor(wp, experiment, 'all' if all_mutants else '')
+
+    if all_mutants:
+        all_suffix = '.all'
+    else:
+        all_suffix = ''
+
+    # equivalence checker must be run before fuzzer.
+    with open(workdir / f"eqvcheck_results{all_suffix}.{experiment}.json", "r") as f:
+        not_equivalent = set(json.load(fp=f))
+
+    if all_mutants:
+        # we still restrict this to non-equivalent mutants?
+        run_on = [x['target'] for x in mutants if x['src'] in not_equivalent]
+    else:
+        survivors = set(survivors)
+        run_on = [x['target'] for x in mutants if x['src'] in survivors and x['src'] in not_equivalent]
+
+    out = []
+    programs = []
+    for p in run_on:
+        mutsrc = workdir / f"libfuzzer_{fuzzer}" / p
+        programs.append(p)
+
+        if parallel:
+            out.append(run_fuzzer_on_mutant(executor, insn, mutsrc))
+        else:
+            out.append(executor.run(insn, mutsrc))
+
+    return finish_fuzzer(workdir, fuzzer, all_suffix, experiment, programs, inputs=out)
+
+
+if __name__ == "__main__":
+    from setup_workdir import WorkParams
+    from runconfig import config
+
+    p = argparse.ArgumentParser(description="Run fuzzer on mutants")
+
+    p.add_argument("workdir", help="Work directory")
+    p.add_argument("experiment", help="Experiment name, must be suitable for embedding in filenames")
+    p.add_argument("--mutator", choices=get_mutators(), default="MUSIC")
+    p.add_argument("--insn", help="Instruction to process, '@FILE' form loads list from file instead")
+    p.add_argument("--all", help="Run the fuzzer on all mutants, not just survivors (NOT RECOMMENDED)", action="store_true")
+    p.add_argument("--fuzzer", help="Choose variant of fuzzer to run",
+                   choices=['simple', 'custom'], default='simple')
+
+    args = p.parse_args()
+    insns = get_instructions(args.insn)
+
+    parsl.load(config)
+
+    if len(insns):
+        wp = WorkParams.load_from(args.workdir)
+        muthelper = get_mutation_helper(args.mutator, wp)
+        pt = PrepTimer()
+
+        pt.start_timer()
+
+        out = []
+        for i in insns:
+            insn = Insn(i)
+            out.append(run_fuzzer(wp, insn, args.experiment, muthelper, all_mutants = args.all, fuzzer=args.fuzzer, parallel = True))
+
+        for i, o in zip(insns, out):
+            print(i)
+            o.result()
+
+        pt.end_timer()
