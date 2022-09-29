565f323bb1c0298799754c5a6afa5f0e52d1c1f0
Sreepathi Pai <sree@cs.rochester.edu>
2022-09-29 12:16:16 -0400

    Add run_mutants_2.py that uses join apps and the high throughput executor to hopefully utilize all cores. Logs get destroyed though. Experimental for now, but should replace run_mutants.py

--- rocettaprep/runconfig.py
+++ rocettaprep/runconfig.py
@@ -4,9 +4,46 @@
 
 from parsl.config import Config
 from parsl.executors.threads import ThreadPoolExecutor
+
+from parsl.providers import LocalProvider
+from parsl.channels import LocalChannel
+from parsl.executors import HighThroughputExecutor
+
 import os
 
 if os.cpu_count() is None:
     config = Config(executors=[ThreadPoolExecutor()])
+
+    htconfig = Config(
+        executors=[
+            HighThroughputExecutor(
+                worker_debug=True,
+                cores_per_worker=1,
+                provider=LocalProvider(
+                    channel=LocalChannel(),
+                    init_blocks=1,
+                    max_blocks=1,
+                ),
+            )
+        ],
+        strategy=None
+        )
 else:
     config = Config(executors=[ThreadPoolExecutor(max_threads=os.cpu_count())])
+
+    htconfig = Config(
+        executors=[
+            HighThroughputExecutor(
+                worker_debug=True,
+                cores_per_worker=1,
+                provider=LocalProvider(
+                    channel=LocalChannel(),
+                    init_blocks=1,
+                    max_blocks=os.cpu_count(),
+                ),
+            )
+        ],
+        strategy=None
+    )
+
+
