2503ee6028253982ffa35aafd96e3509475e0d78
Sreepathi Pai <sree@cs.rochester.edu>
2022-09-29 14:23:58 -0400

    Add run_eqvcheck_2.py that improves scalability, move cbmc executor out to aid in unpickling.

---
+++ rocettaprep/cbmc_executor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#
+# separated out to aid pickling.
+import itertools
+from runcommon import run_and_time
+import os
+import sys
+
+# https://github.com/diffblue/cbmc/blob/48893287099cb5780302fe9dc415eb6888354fd6/src/util/exit_codes.h
+CBMC_RC_OK = 0
+CBMC_RC_PARSE_ERROR = 2
+CBMC_RC_CONV_ERROR = 6
+CBMC_RC_VERIFICATION_UNSAFE = 10 # also conversion error when writing to other file.
+
+class CBMCExecutor:
+    def __init__(self, wp, experiment, subset = ''):
+        self.wp = wp
+        self.experiment = experiment
+        self.subset = subset
+
+    def run(self, insn, mutant):
+        xinc = list(zip(itertools.repeat("-I"), self.wp.include_dirs))
+
+        if self.subset:
+            subset = self.subset + '.'
+        else:
+            subset = ''
+
+        ofile = mutant.parent / f"cbmc_output.{mutant.name}.{subset}{self.experiment}.json"
+        cmd = ["cbmc", "--unwind", str(64), "--unwinding-assertions", "--z3", "--json-ui", "--trace", "-I", str(self.wp.csemantics.parent)]
+        cmd.extend(xinc)
+        cmd.append(str(mutant))
+        h = os.open(ofile, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, mode=0o666)
+        print(" ".join(cmd))
+        r, t = run_and_time(cmd, stdout=h, timeout_s = 5*60)
+        if t is not None:
+            print(f"{insn.insn}:{mutant}:{subset}{self.experiment}: Equivalence checker took {t/1E6} ms, retcode={r.returncode}", file=sys.stderr)
+        else:
+            print(f"{insn.insn}:{mutant}:{subset}{self.experiment}: Equivalence checker timed out, retcode={r.returncode}", file=sys.stderr)
+
+        os.close(h)
+        return {'time_ns': t, 'retcode': r.returncode}
---
+++ rocettaprep/run_eqvcheck_2.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+#
+# run_eqvcheck.py
+#
+# Run and generate equivalence checker output.
+
+
+import subprocess
+import argparse
+from roctest import *
+from rocprepcommon import *
+from build_single_insn import Insn
+import json
+import subprocess
+from mutate import MUSICHelper, get_mutation_helper, get_mutators
+import itertools
+import os
+import json
+
+from parsl.app.app import python_app, join_app
+import parsl
+
+import sys
+import time
+
+from cbmc_executor import *
+
+@python_app
+def run_cbmc(cbmc, insn, mutsrc):
+    return cbmc.run(insn, mutsrc)
+
+@python_app
+def finish_eqv_check(workdir, all_mutants, experiment, programs = [], inputs = []):
+    import json
+    from run_eqvcheck_2 import CBMC_RC_VERIFICATION_UNSAFE
+
+    out = list(zip(programs, inputs))
+    results = [x for x, y in out if y['retcode'] == CBMC_RC_VERIFICATION_UNSAFE]
+
+    if all_mutants:
+        resfile = workdir / f"eqvcheck_results.all.{experiment}.json"
+        timfile = workdir / f"eqvcheck_timing.all.{experiment}.json"
+    else:
+        resfile = workdir / f"eqvcheck_results.{experiment}.json"
+        timfile = workdir / f"eqvcheck_timing.{experiment}.json"
+
+    with open(resfile, "w") as f:
+        json.dump(results, fp=f)
+
+    with open(timfile, "w") as f:
+        json.dump(dict(out), fp=f)
+
+@join_app
+def run_eqv_check(wp, insn, experiment, muthelper, all_mutants = False, parallel = True):
+    import json
+    from roctest import InsnTest
+
+    tt = InsnTest(wp, insn)
+
+    workdir = wp.workdir / insn.working_dir
+
+    mutants = muthelper.get_mutants(insn)
+    survivors = muthelper.get_survivors(insn, experiment)
+
+    executor = CBMCExecutor(wp, experiment, 'all' if all_mutants else '')
+
+    if all_mutants:
+        run_on = [x['src'] for x in mutants]
+    else:
+        run_on = survivors
+
+    out = []
+    programs = []
+
+    for p in run_on:
+        mutsrc = workdir / "eqchk" / p
+        programs.append(p)
+        if parallel:
+            out.append(run_cbmc(executor, insn, mutsrc))
+        else:
+            out.append(executor.run(insn, mutsrc))
+
+    return finish_eqv_check(workdir, all_mutants, experiment, programs, inputs=out)
+
+if __name__ == "__main__":
+    from setup_workdir import WorkParams
+    from runconfig import htconfig
+
+    p = argparse.ArgumentParser(description="Run equivalence tests on mutants")
+
+    p.add_argument("workdir", help="Work directory")
+    p.add_argument("experiment", help="Experiment name, must be suitable for embedding in filenames")
+    p.add_argument("--mutator", choices=get_mutators(), default="MUSIC")
+    p.add_argument("--insn", help="Instruction to process, '@FILE' form loads list from file instead")
+    p.add_argument("--all", help="Run equivalence checks on all mutants, not just survivors", action="store_true")
+
+    args = p.parse_args()
+    insns = get_instructions(args.insn)
+
+    parsl.load(htconfig)
+
+    if len(insns):
+        wp = WorkParams.load_from(args.workdir)
+        muthelper = get_mutation_helper(args.mutator, wp)
+        pt = PrepTimer()
+        pt.start_timer()
+
+        out = []
+        for i in insns:
+            insn = Insn(i)
+            out.append(run_eqv_check(wp, insn, args.experiment, muthelper, all_mutants = args.all, parallel = True))
+
+        for i, o in zip(insns, out):
+            print(i)
+            o.result()
+
+        pt.end_timer()
